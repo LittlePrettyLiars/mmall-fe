d13ba37d06cec1639ad4b5c4c63c39f3cf3773d3
“郭 <cherry_guo923@163.com>
2018-06-01 11:24:27 +0800

    啊呀妈呀疯了，以前push过

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -3,17 +3,9 @@
 <<<<<<< HEAD
 * @Date:   2018-05-30 15:38:57
 * @Last Modified by:   E2
-* @Last Modified time: 2018-05-30 17:59:44
+* @Last Modified time: 2018-06-01 11:23:16
 */
 
 require('./index.css');
 require('../module.js');
-=======
-* @Date:   2017-12-22 11:30:37
-* @Last Modified by:   E2
-* @Last Modified time: 2017-12-25 16:37:19
-*/
-'use strict';
-require('./index.css');
-require('../module.js');
->>>>>>> 474ba58c33ee09687b97fe31c311d990353d7368
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 /*
 * @Author: E2
-<<<<<<< HEAD
 * @Date:   2018-05-30 15:35:14
 * @Last Modified by:   E2
-* @Last Modified time: 2018-06-01 11:03:46
+* @Last Modified time: 2018-06-01 11:21:22
 */
 var webpack           = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -63,68 +62,3 @@ if(WEBPACK_ENV == 'dev'){
 }
 
 module.exports = config;
-=======
-* @Date:   2017-12-22 11:59:16
-* @Last Modified by:   E2
-* @Last Modified time: 2017-12-25 22:04:17
-*/
-var webpack           = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
-//环境变量，dev/online
-var WEBPACk_ENV       = process.env.WEBPACk_ENV || 'dev';
-console.log(WEBPACk_ENV);
-
-//获取html-webpack-plugin参数的方法
-var getHtmlConfig = function(name){
-    return{
-        template: './src/view/'+ name +'.html',
-        filename: 'view/'+ name +'.html',
-        inject  : true,
-        hash    : true,
-        chunks  : ['common', name ]
-    }
-};
-
-//webpack.config
-var config = {
-     entry: {
-     	'common' : ['./src/page/common/index.js'],
-     	'index'  : ['./src/page/index/index.js'],
-     	'login'  : ['./src/page/login/index.js']
-     },
-     output: {
-         path      : './dist',
-         publicPath: '/dist',
-         filename  : 'js/[name].js'
-     },
-     externals : {
-     	'jquery' : 'window.jQuery'
-     },
-     module: {
-       loaders: [
-         { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader","css-loader") },
-         { test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]'},
-         { test: /\.string$/, loader: 'html-loader'}
-       ]
-     },
-     plugins : [
-        //独立通用模块到js/base.js
-     	new webpack.optimize.CommonsChunkPlugin({
-     		name : 'common',
-     		filename : 'js/base.js'
-     	}),
-        //把CSS单独打包到文件里
-        new ExtractTextPlugin("css/[name].css"),
-        //html模板的处理
-        new HtmlWebpackPlugin(getHtmlConfig('index')),
-        new HtmlWebpackPlugin(getHtmlConfig('login')),
-     ]
- };
-
-if(WEBPACk_ENV === 'dev'){
-    config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
-}
- module.exports = config;
->>>>>>> 474ba58c33ee09687b97fe31c311d990353d7368
