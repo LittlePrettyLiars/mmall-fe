dad632996ead35b8eab3eefb7d2754697df37e07
“郭 <cherry_guo923@163.com>
2018-07-31 11:30:29 +0800

    商品和支付模块

---
+++ src/util/slider/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: E2
+* @Date:   2017-10-27 10:45:01
+* @Last Modified by:   E2
+* @Last Modified time: 2017-10-27 10:46:04
+*/
+
+'use strict';
+require('./index.css');
+require('./unslider.js');
